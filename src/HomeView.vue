1e6b99311ff397ddb1c25128fdc1a9e5e5d592a1
Fabio Costa <fabioasync@gmail.com>
2024-03-26 14:54:52 -0300

    initial commit

---
+++ src/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+   <div class="layout">
+      <main>
+         <div class="card position-dialog p-4 rounded-xl bg-slate-900/70">
+            <div class="flex flex-col">
+               <span>Welcome to our user site!</span>
+               <span>
+                  This is a simple page for
+                  <strong style="color: #ffc900">Sintia</strong>
+                  testing purposes.
+               </span>
+            </div>
+
+            <div
+               class="bg-slate-300 text-slate-800 text-center mt-4 rounded-lg py-1 flex items-center justify-between px-3 gap-x-3">
+               <span class="text-sm text-slate-800">
+                  &copy; 2024 Site Labs. All rights reserved.
+               </span>
+               <span class="leading-none">
+                  <a
+                     class="text-sm link text-info"
+                     href="https://github.com/72fcosta"
+                     target="_blank">
+                     @72fcosta
+                  </a>
+               </span>
+            </div>
+         </div>
+      </main>
+   </div>
+</template>
